f3e9a2ea103af708a7c1fcbaf61b1de22ada808e
AleksaGavric <aleksagavric1@gmail.com>
2022-06-14 22:44:02 -0400

    feat: got temp

--- get_weather.py
+++ get_weather.py
@@ -14,14 +14,29 @@ def get_weather_data(url):
     soup = bs(html.text, "html.parser")
     
     result = {}
+    will_rain = True;
+    
     result['dayhour'] = soup.find("div", attrs={"id": "wob_dts"}).text
     result['weather_now'] = soup.find("span", attrs={"id": "wob_dc"}).text
+    result['precipitation'] = soup.find("span", attrs={"id": "wob_pp"}).text
     
-    return result
+    days = soup.find("div", attrs={"id": "wob_dp"})
+    day = days.findAll("div", attrs={"class": "wob_df"})[0]
+    temp = day.findAll("span", {"class": "wob_t"})
+    
+    result['max-temp'] = temp[0].text
+    result['min-temp'] = temp[2].text
+    
+    return result, will_rain
 
 if __name__ == "__main__":
     URL = "https://www.google.com/search?q=charlotte+weather&oq=charlotte+weather&aqs=chrome..69i57.2478j0j4&sourceid=chrome&ie=UTF-8"
-    data = get_weather_data(URL)
+    data, rain_check = get_weather_data(URL)
     
-    print("\nNow:", data["dayhour"])
-    print("Description:", data['weather_now'])
+    message = ""
+    message += "Min temp: {0} Fah, {1} Cel".format(data['min-temp'], int((int (data['min-temp']) - 32) * 5.0/9.0))
+    message += "\nMax temp: {0} Fah, {1} Cel".format(data['max-temp'], int((int (data['max-temp']) - 32) * 5.0/9.0))
+    
+    if (rain_check):
+        print(message)
+    
